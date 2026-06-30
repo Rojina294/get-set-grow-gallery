b85cc962b7e85b193bea860fc17ea9acb7c80811
Rojina294 <rojinag294@gmail.com>
2026-07-01 00:24:10 +1000

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,5 @@
 const CLOUDINARY_CLOUD_NAME = "e5hamkjp";
 const CLOUDINARY_UPLOAD_PRESET = "GetSetGrow_Gallery";
 
-// 4-digit PIN login
-const EDUCATOR_PASSWORD = "7294";   // Educator mode
-const PARENT_PASSWORD = "4816";     // Parent mode
+const EDUCATOR_PASSWORD = "7294";
+const PARENT_PASSWORD = "4816";
